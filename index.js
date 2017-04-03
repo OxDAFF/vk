e2b40b7bd672833d286b2a82b279a312a11f67ba
OxDAFF <mail.mr.samoilov@gmail.com>
2017-04-03 14:00:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var app = express();
-var VK = require('vksdk');
-var vk = new VK({
-   'appId'     : 5963094,
-   'appSecret' : 'ToBbOS0OAOMiDy2yX9Y7',
-   'language'  : 'ru'
-});
+
 app.set('port', (process.env.PORT || 5000));
 
 
@@ -14,24 +9,42 @@ app.set('port', (process.env.PORT || 5000));
  * Request server methods
  */
 app.get('/', function(req, res) {
-// Setup server access token for server API methods 
-    vk.on('serverTokenReady', function(_o) {
-    // Here will be server access token 
-        vk.setToken(_o.access_token);
-    });
- 
-// Turn on requests with access tokens 
-    vk.setSecureRequests(true);
- 
-// Request server API method 
+var VK = require('vksdk');
+var vk = new VK({
+   'appId'     : 5963094,
+   'appSecret' : 'ToBbOS0OAOMiDy2yX9Y7',
+   'language'  : 'ru'
+});
+/**
+ * Request server methods
+ */
 
-    vk.request('secure.getSMSHistory', {}, function(_dd) {
-      res.send(_dd);
-    });
-   vk.setToken(access_token)
-   vk.request('users.get', {'user_id' : 1}, function(_o) {
-      res.send(_o);
-   });
+// Setup server access token for server API methods
+vk.on('serverTokenReady', function(_o) {
+    // Here will be server access token
+    vk.setToken(_o.access_token);
+});
+
+// Turn on requests with access tokens
+vk.setSecureRequests(true);
+
+// Request server API method
+vk.request('secure.getSMSHistory', {}, function(_dd) {
+    res.send('ghjhjhj dfgf');
+    res.send(_dd);
+});
+
+/**
+ * Request client methods
+ */
+// First you have to pass access_token from client side JS code
+vk.setToken(access_token);
+
+// Request 'users.get' method
+vk.request('users.get', {'user_id' : 1}, function(_o) {
+    res.send('222222 222222');
+    res.send(_o);
+});
 });
  
 /**
